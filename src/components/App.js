bd915ebc4f295b774171de5bb374675f29081c09
Laura733 <lali733@gmail.com>
2021-06-08 08:39:56 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../stylesheets/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hola Mundo</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
