cddfa47483a6268981a024056d4aa6ceb62eb91c
Laura733 <lali733@gmail.com>
2021-06-11 09:56:11 +0200

    Add title

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
 import "../stylesheets/App.scss";
+//import React, { useState } from "react"; para guardar la data en el estado
 import data from "../data/data.json";
 import PokeList from "./PokeList";
 
 function App() {
+  //const [dataObject, setDataObject] =(data); useState para guardar en el estado pero no hace falta la función del cambio de estado (setDataObject) porque no se utilizaría.
+
   const state = {
     dataObject: data,
   };
   return (
     <>
       <PokeList data={state.dataObject} />
-      
+      {/* <PokeList data={dataObject} /> */}
     </>
-    
   );
 }
 
--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -4,20 +4,23 @@ import Pokemon from "./Pokemon";
 
 function PokeList(props) {
   return (
-    <ul className="poke__list">
-      {props.data.map((pokemon) => {
-        return (
-          <li className="poke__list--card" key={pokemon.id}>
-            <Pokemon
-              id={pokemon.id}
-              name={pokemon.name}
-              types={pokemon.types}
-              url={pokemon.url}
-            />
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <h1 className='poke__list--title'> Mi lista de Pokemons </h1>
+      <ul className='poke__list'>
+        {props.data.map((pokemon) => {
+          return (
+            <li className='poke__list--card' key={pokemon.id}>
+              <Pokemon
+                id={pokemon.id}
+                name={pokemon.name}
+                types={pokemon.types}
+                url={pokemon.url}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
